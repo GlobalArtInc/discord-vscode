7a7d12d94280f3609bccf48a5f8e226efb48f97b
Roman Khabarov <roman@globalart.dev>
2023-07-09 11:53:41 +0300

    fix: clintId

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 import LANG from './data/languages.json';
 
-export const CLIENT_ID = '383226320970055681' as const;
+export const CLIENT_ID = '1127365366977396867' as const;
 
 export const KNOWN_EXTENSIONS: { [key: string]: { image: string } } = LANG.KNOWN_EXTENSIONS;
 export const KNOWN_LANGUAGES: { language: string; image: string }[] = LANG.KNOWN_LANGUAGES;
