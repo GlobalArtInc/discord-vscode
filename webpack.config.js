44839604f0163f05af39b1a6a639ab4407b5e425
Roman Khabarov <roman@globalart.dev>
2023-07-12 23:32:24 +0300

    ci: fix linter

--- webpack.config.js
+++ webpack.config.js
@@ -4,44 +4,44 @@ const TerserPlugin = require('terser-webpack-plugin');
 
 /** @type {import('webpack').Configuration} */
 module.exports = {
-  target: 'node',
-  entry: './src/extension.ts',
-  output: {
-    filename: 'extension.js',
-    libraryTarget: 'commonjs2',
-    path: path.resolve(process.cwd(), 'dist'),
-  },
-  devtool: 'source-map',
-  externals: {
-    vscode: 'commonjs vscode',
-  },
-  resolve: {
-    extensions: ['.ts', '.js', '.json'],
-  },
-  plugins: [new CleanWebpackPlugin()],
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new TerserPlugin({
-        extractComments: true,
-        terserOptions: {
-          output: {
-            comments: false,
-          },
-          mangle: false,
-          keep_classnames: true,
-          keep_fnames: true,
-        },
-      }),
-    ],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
-  }
+	target: 'node',
+	entry: './src/extension.ts',
+	output: {
+		filename: 'extension.js',
+		libraryTarget: 'commonjs2',
+		path: path.resolve(process.cwd(), 'dist'),
+	},
+	devtool: 'source-map',
+	externals: {
+		vscode: 'commonjs vscode',
+	},
+	resolve: {
+		extensions: ['.ts', '.js', '.json'],
+	},
+	plugins: [new CleanWebpackPlugin()],
+	optimization: {
+		minimize: true,
+		minimizer: [
+			new TerserPlugin({
+				extractComments: true,
+				terserOptions: {
+					output: {
+						comments: false,
+					},
+					mangle: false,
+					keep_classnames: true,
+					keep_fnames: true,
+				},
+			}),
+		],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				use: 'ts-loader',
+				exclude: /node_modules/,
+			},
+		],
+	},
 };
